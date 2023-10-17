f5a462449bccec4df28f03bb41d3181bf9606fff
banskt <banskt.saikat@gmail.com>
2023-10-17 18:47:12 +0000

    Housekeeping ashwini

--- sysconfig/ashwini-ihostart-vps-storage/scripts/fetch-btn-feed.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source /home/banskt/.bashrc
-micromamba activate py311
-python local/apps/btn-automagic/fetch-btn-feed.py
-echo "Fetched database from BTN"
-micromamba deactivate
