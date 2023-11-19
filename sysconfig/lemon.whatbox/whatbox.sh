79d437e2375efc4ef5cb14d3f80f53b2ceeb0235
banskt <banskt.saikat@gmail.com>
2023-11-19 17:55:14 +0000

    Scripts for whatbox

--- sysconfig/lemon.whatbox/whatbox.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Per-user tmp directory
-# This will set $TMPDIR and create ~/.tmp for temporary files
-. /etc/profile.d/01tmpdir.sh
-
-# Put your fun stuff here.
-# Enables ^s and ^q in rTorrent, when running in screen
-stty -ixon -ixoff
